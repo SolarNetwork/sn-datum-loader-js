29cbf56ec52eb257cadecd32eba57621991da16c
Matt Magoffin <matt@solarnetwork.net>
2017-08-16 19:45:04 +1200

    Add missing export in lib.js.

--- lib.js
+++ lib.js
@@ -1,3 +1,4 @@
 export { default as DatumRangeFinder } from './src/datumRangeFinder';
+export { default as DatumSourceFinder } from './src/datumSourceFinder';
 export { default as DatumLoader } from './src/datumLoader';
 export { default as MultiLoader } from './src/multiLoader';
