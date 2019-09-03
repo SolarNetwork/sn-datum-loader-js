e3b6864399fcfee8c0c3c6fcbdf9086cc5ca3aec
Matt Magoffin <matt@solarnetwork.net>
2019-09-04 06:37:26 +1200

    Update development deps to pick up security alert fixes.

--- rollup.config.js
+++ rollup.config.js
@@ -1,36 +1,41 @@
-import babel from 'rollup-plugin-babel';
-import includePaths from 'rollup-plugin-includepaths';
+import babel from "rollup-plugin-babel";
+import includePaths from "rollup-plugin-includepaths";
 
 const includePathOptions = {
-  include: {},
-  paths: ['src'],
-  external: [],
-  extensions: ['.js']
+	include: {},
+	paths: ["src"],
+	external: [],
+	extensions: [".js"]
 };
 
 export default {
-  external: id => {
-    return /(d3-|solarnetwork-api-)/.test(id);
-  },
-  globals: {
-    'd3-queue': 'd3',
-    'd3-request': 'd3',
-    'solarnetwork-api-core': 'sn',
-  },
-  plugins: [
-    includePaths(includePathOptions),
-    babel({
-      exclude: 'node_modules/**',
-      babelrc: false,
-      plugins: ['external-helpers'],
-      presets: [
-        ['env', {
-          targets: {
-            browsers: ['last 2 versions'],
-            node: 'current',
-          },
-          modules: false,
-        }]
-      ]
-    })]
+	external: id => {
+		return /(d3-|solarnetwork-api-)/.test(id);
+	},
+	output: {
+		globals: {
+			"d3-queue": "d3",
+			"d3-request": "d3",
+			"solarnetwork-api-core": "sn"
+		}
+	},
+	plugins: [
+		includePaths(includePathOptions),
+		babel({
+			exclude: "node_modules/**",
+			babelrc: false,
+			presets: [
+				[
+					"@babel/env",
+					{
+						targets: {
+							browsers: ["last 2 versions"],
+							node: "current"
+						},
+						modules: false
+					}
+				]
+			]
+		})
+	]
 };
