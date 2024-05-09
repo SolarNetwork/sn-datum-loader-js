bcb58548f70b41bf2609fb32f3215daa646a8d52
Matt Magoffin <matt@solarnetwork.net>
2024-05-09 16:00:06 +1200

    Export DatumRangeFinder.

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,12 +1,20 @@
 import * as FetchApi from "./fetch.js";
 import { Datum, LoaderDataCallbackFn, Loader } from "./loader.js";
-import { default as DatumLoader } from "./datumLoader.js";
 import JsonClientSupport from "./jsonClientSupport.js";
+import DatumLoader from "./datumLoader.js";
+import {
+	default as DatumRangeFinder,
+	DatumRange,
+	DatumRangeResult,
+} from "./datumRangeFinder.js";
 export {
 	FetchApi,
 	type Datum,
 	DatumLoader,
-	type LoaderDataCallbackFn as DatumLoaderDataCallbackFn,
+	DatumRangeFinder,
+	type DatumRange,
+	type DatumRangeResult,
+	type LoaderDataCallbackFn,
 	JsonClientSupport,
 	type Loader,
 };
