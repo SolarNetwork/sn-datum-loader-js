63694d865c378f4b4d3db220d4154ba9b77edb7b
Matt Magoffin <matt@solarnetwork.net>
2025-04-04 08:17:09 +1300

    Keep solarnetwork-api-core as external.

--- rollup.config.cjs.mjs
+++ rollup.config.cjs.mjs
@@ -3,6 +3,9 @@ import commonjs from "@rollup/plugin-commonjs";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 
 export default {
+	external: (id) => {
+		return /solarnetwork-api-core/.test(id);
+	},
 	input: "src/main/index.ts",
 	output: {
 		format: "cjs",
